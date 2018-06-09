1ad263d1363d723c4c70e537af88e48dfadc3480
lorraine rono <lorrainerono2017@gmail.com>
2018-06-09 20:53:11 +0300

    added butterknife

--- app/src/main/java/com/epicodus/splatter/HomeActivity.java
+++ app/src/main/java/com/epicodus/splatter/HomeActivity.java
@@ -2,12 +2,30 @@ package com.epicodus.splatter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class HomeActivity extends AppCompatActivity {
+
+    public class HomeActivity extends AppCompatActivity {
+        @Bind(R.id.listView)
+        ListView mListView;
+        private String[] images = new String[] {"Mi Mero Mole", "Mother's Bistro",
+                "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+                "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+                "Lardo", "Portland City Grill", "Fat Head's Brewery",
+                "Chipotle", "Subway"};
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this,  android.R.layout.simple_list_item_1, images);
+            mListView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/epicodus/splatter/MainActivity.java
+++ app/src/main/java/com/epicodus/splatter/MainActivity.java
@@ -5,21 +5,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
+
 
 public class MainActivity extends AppCompatActivity {
-    private Button mStartDiscoveryButton;
+    @Bind(R.id.startDiscoveryButton)
+    Button mStartDiscoveryButton;
+    @Bind(R.id.appNameTextView)
+    TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mStartDiscoveryButton = (Button) findViewById(R.id.startDiscoveryButton);
-        mStartDiscoveryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+        mStartDiscoveryButton.setOnClickListener(this);
+        @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "I Know Youll Love It!!", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this , HomeActivity.class);
                 startActivity(intent);
+
             }
-        });
+        }
     }
-}
