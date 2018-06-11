3ede52270b2f9971ada5a34c22fb9958f18030b8
lorraine rono <lorrainerono2017@gmail.com>
2018-06-11 16:22:16 +0300

    added OkHTTP request

---
+++ app/src/main/java/com/epicodus/splatter/Constants.java
@@ -0,0 +1,12 @@
+package com.epicodus.splatter;
+
+import java.security.PublicKey;
+
+public class Constants {
+    public static final String UNSPLASH_ACCESS_KEY = BuildConfig.UNSPLASH_ACCESS_KEY;
+    public static final String UNSPLASH_ID = BuildConfig.UNSPLASH_ID;
+    public static final String UNSPLASH_TOKEN_SECRET = BuildConfig.UNSPLASH_TOKEN_SECRET;
+    public static final String UNSPLASH_BASE_URL = "https://api.unsplash.com/search/photos";
+    public static final String UNSPLASH_NAME_QUERY_PARAMETER = "name";
+
+}
