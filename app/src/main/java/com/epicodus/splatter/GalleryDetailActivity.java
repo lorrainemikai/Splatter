c4aaa0891fa5bb37771f58f390ef3abb4dd47145
lorraine rono <lorrainerono2017@gmail.com>
2018-06-19 12:53:58 +0300

    added fragments and details

---
+++ app/src/main/java/com/epicodus/splatter/GalleryDetailActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.splatter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalleryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_detail);
+    }
+}
